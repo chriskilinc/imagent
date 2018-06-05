b7561c937a69f70a141bc0bef58141d956ea67c1
chriskilinc <chriskilinc@gmail.com>
2018-06-05 22:55:49 +0200

    Added Twit Stream on 'follow' event

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const Twit = require('twit');
 const twitterConfig = require('./twitterConfig');
-console.log("[imagent] Starting Imagent");
+console.log("[imagent]  Starting Imagent");
+
+//  Twitter Config
+var T = new Twit(twitterConfig);
+
+//  Create a USER stream on FOLLOW event
+var tStream = T.stream('user');
+tStream.on('follow', followed);
+
+function followed(eventData){
+  console.log("[T]  Someone Followed");
+}
