422c4875c7fcd46d83c36f6557c130c25e2b61f7
chriskilinc <chriskilinc@gmail.com>
2018-06-05 23:02:43 +0200

    Saves followed users screen name and name. Then prints it out to the console.

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ var T = new Twit(twitterConfig);
 var tStream = T.stream('user');
 tStream.on('follow', followed);
 
+let followerName;
+let followerScreenName;
 function followed(eventData){
   console.log("[T]  Someone Followed");
+  followerName = eventData.source.name;
+  followerScreenName = eventData.source.screen_name;
+  console.log("@"+followerScreenName+" - '"+followerName+"'");
+
+  //  Exexcute Program Processing
 }
